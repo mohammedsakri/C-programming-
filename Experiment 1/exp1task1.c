fceef26c95c7487b54ffe3eab1300cf5b904e68d
mohammedsakri <sakrimohammed@eng.rizvi.edu.in>
2024-11-28 13:56:35 +0530

    Add files via upload

---
+++ Experiment 1/exp1task1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+ printf("Full Name\t: Mohammed Harish Sakri \n");
+printf("Age\t\t:18\n");
+printf("class\t\t:FE\n");
+printf("Division\t:A\n");
+printf("UIN\t\t:241A030");
+
+ return 0;
+}
